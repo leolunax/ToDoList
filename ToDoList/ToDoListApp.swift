83d9f21b6b4d09f14c076f6a3a44f0c3fce70895
Leonardo Luna <93144721+leolunax@users.noreply.github.com>
2024-03-01 21:50:38 -0800

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Leonardo Luna on 3/1/24.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
